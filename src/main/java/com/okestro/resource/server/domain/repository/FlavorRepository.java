cbf6ee49df6a75ef90c458620b30465ac5d89d08
belljun3395 <195850@jnu.ac.kr>
2025-05-26 17:27:06 +0900

    feat: server 필요 repository 정의

---
+++ src/main/java/com/okestro/resource/server/domain/repository/FlavorRepository.java
@@ -0,0 +1,6 @@
+package com.okestro.resource.server.domain.repository;
+
+import com.okestro.resource.server.domain.FlavorEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FlavorRepository extends JpaRepository<FlavorEntity, Long> {}
