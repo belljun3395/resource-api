d0cb404a99c0042f5a08887a35835f6d74bf1b94
belljun3395 <195850@jnu.ac.kr>
2025-05-26 13:45:39 +0900

    공통 ServerController 선언 (#2)

---
+++ src/main/java/com/okestro/resource/server/controller/ServerController.java
@@ -0,0 +1,10 @@
+package com.okestro.resource.server.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/v1/servers")
+@RequiredArgsConstructor
+@RestController
+public class ServerController {}
