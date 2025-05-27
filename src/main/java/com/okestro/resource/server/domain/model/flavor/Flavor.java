5777ff83de8bae25aec9a4b525fc4690c21cc93c
belljun3395 <195850@jnu.ac.kr>
2025-05-27 22:23:24 +0900

    Server 초기 모델 구현 (#15)
    * feat: flavor 모델 구현
    
    * feat: instance 모델 구현

---
+++ src/main/java/com/okestro/resource/server/domain/model/flavor/Flavor.java
@@ -0,0 +1,35 @@
+package com.okestro.resource.server.domain.model.flavor;
+
+import com.okestro.resource.server.domain.FlavorEntity;
+import java.time.LocalDateTime;
+import lombok.Getter;
+
+@Getter
+public class Flavor extends BaseFlavor {
+	private final Long id;
+	private final LocalDateTime createdAt;
+
+	private Flavor(
+			String name,
+			String description,
+			Float vCpu,
+			Float memory,
+			Float rootDiskSize,
+			Long id,
+			LocalDateTime createdAt) {
+		super(name, description, vCpu, memory, rootDiskSize);
+		this.id = id;
+		this.createdAt = createdAt;
+	}
+
+	public static Flavor from(FlavorEntity flavorEntity) {
+		return new Flavor(
+				flavorEntity.getName(),
+				flavorEntity.getDescription(),
+				flavorEntity.getVCpu(),
+				flavorEntity.getMemory(),
+				flavorEntity.getRootDiskSize(),
+				flavorEntity.getId(),
+				flavorEntity.getCreatedAt());
+	}
+}
