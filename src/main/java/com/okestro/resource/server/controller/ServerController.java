b8ea38f77bf5c2d8e5d1b3b59d096ee955c5f8db
belljun3395 <195850@jnu.ac.kr>
2025-05-27 15:00:34 +0900

    feat: PostInstanceUseCase API 개발

--- src/main/java/com/okestro/resource/server/controller/ServerController.java
+++ src/main/java/com/okestro/resource/server/controller/ServerController.java
@@ -1,10 +1,28 @@
 package com.okestro.resource.server.controller;
 
+import com.okestro.resource.server.application.PostInstanceUseCase;
+import com.okestro.resource.server.application.dto.PostInstanceUsecaseDto;
+import com.okestro.resource.server.controller.request.PostInstanceRequest;
+import com.okestro.resource.support.web.ApiResponse;
+import com.okestro.resource.support.web.ApiResponseGenerator;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api/v1/servers")
 @RequiredArgsConstructor
 @RestController
-public class ServerController {}
+public class ServerController {
+	private final PostInstanceUseCase postInstanceUseCase;
+
+	@PostMapping("/instance")
+	public ApiResponse<ApiResponse.SuccessBody<PostInstanceUsecaseDto.PostInstanceUseCaseOut>>
+			postInstance(@RequestBody PostInstanceRequest request) {
+		PostInstanceUsecaseDto.PostInstanceUseCaseOut useCaseOut =
+				postInstanceUseCase.execute(PostInstanceUsecaseDto.in(request));
+		return ApiResponseGenerator.success(useCaseOut, HttpStatus.CREATED);
+	}
+}
