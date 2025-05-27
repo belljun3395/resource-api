14df1915cf30f0bc016bcaefa6a91cd463809d33
belljun3395 <195850@jnu.ac.kr>
2025-05-27 18:10:50 +0900

    Server 이벤트 초기 구현 (#11)
    * feat: ServerEventPublisher 구현
    
    * feat: InstanceEvent 선언

---
+++ src/main/java/com/okestro/resource/server/event/ServerEventPublisher.java
@@ -0,0 +1,11 @@
+package com.okestro.resource.server.event;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ServerEventPublisher {
+    private final ApplicationEventPublisher applicationEventPublisher;
+}
