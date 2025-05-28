17d07686519fbacd208145c18b192ae51f0ba9bf
belljun3395 <195850@jnu.ac.kr>
2025-05-28 16:24:02 +0900

    feat: UpdateInstancePowerUseCase API 개발

--- src/main/java/com/okestro/resource/server/controller/ServerController.java
+++ src/main/java/com/okestro/resource/server/controller/ServerController.java
@@ -1,10 +1,29 @@
 package com.okestro.resource.server.controller;
 
+import com.okestro.resource.server.application.UpdateInstancePowerUseCase;
+import com.okestro.resource.server.application.dto.UpdateInstancePowerUsecaseDto;
+import com.okestro.resource.server.controller.request.UpdateInstancePowerRequest;
+import com.okestro.resource.support.web.ApiResponse;
+import com.okestro.resource.support.web.ApiResponseGenerator;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api/v1/servers")
 @RequiredArgsConstructor
 @RestController
-public class ServerController {}
+public class ServerController {
+	private final UpdateInstancePowerUseCase updateInstancePowerUseCase;
+
+	@PutMapping("/instance/power")
+	public ApiResponse<
+					ApiResponse.SuccessBody<UpdateInstancePowerUsecaseDto.UpdateInstanceUseCaseOut>>
+			updateInstancePower(@RequestBody UpdateInstancePowerRequest request) {
+		UpdateInstancePowerUsecaseDto.UpdateInstanceUseCaseOut useCaseOut =
+				updateInstancePowerUseCase.execute(UpdateInstancePowerUsecaseDto.in(request));
+		if (useCaseOut.getIsUpdated()) {
+			return ApiResponseGenerator.success(useCaseOut, HttpStatus.OK);
+		}
+		return ApiResponseGenerator.success(useCaseOut, HttpStatus.NOT_MODIFIED);
+	}
+}
