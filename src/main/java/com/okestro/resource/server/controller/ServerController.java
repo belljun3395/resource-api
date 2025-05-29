235ad6138cfa9372335343c40a1ff0c1784d7e0d
belljun3395 <195850@jnu.ac.kr>
2025-05-29 14:44:19 +0900

    feat: DeleteInstanceUseCase API 개발

--- src/main/java/com/okestro/resource/server/controller/ServerController.java
+++ src/main/java/com/okestro/resource/server/controller/ServerController.java
@@ -1,10 +1,29 @@
 package com.okestro.resource.server.controller;
 
+import com.okestro.resource.server.application.DeleteInstanceUseCase;
+import com.okestro.resource.server.application.dto.DeleteInstanceUseCaseDto;
+import com.okestro.resource.server.controller.request.DeleteInstanceRequest;
+import com.okestro.resource.support.web.ApiResponse;
+import com.okestro.resource.support.web.ApiResponseGenerator;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api/v1/servers")
 @RequiredArgsConstructor
 @RestController
-public class ServerController {}
+public class ServerController {
+	private final DeleteInstanceUseCase deleteInstanceUseCase;
+
+	@DeleteMapping("/instances")
+	public ApiResponse<ApiResponse.SuccessBody<DeleteInstanceUseCaseDto.DeleteInstanceUseCaseOut>>
+			deleteInstance(@RequestBody DeleteInstanceRequest request) {
+		DeleteInstanceUseCaseDto.DeleteInstanceUseCaseOut useCaseOut =
+				deleteInstanceUseCase.execute(DeleteInstanceUseCaseDto.in(request));
+
+		if (!useCaseOut.getIsDeleted()) {
+			return ApiResponseGenerator.success(useCaseOut, HttpStatus.ACCEPTED);
+		}
+		return ApiResponseGenerator.success(useCaseOut, HttpStatus.OK);
+	}
+}
