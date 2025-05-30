9cb537fe1884557c7aefc824ff3a961f13c130a8
belljun3395 <195850@jnu.ac.kr>
2025-05-30 14:17:10 +0900

    chore: 린트 적용

--- src/main/java/com/okestro/resource/server/domain/InstanceEntity.java
+++ src/main/java/com/okestro/resource/server/domain/InstanceEntity.java
@@ -107,27 +107,27 @@ public class InstanceEntity {
 				.build();
 	}
 
-    @Override
-    public final boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null) return false;
-        Class<?> oEffectiveClass =
-                o instanceof HibernateProxy
-                        ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass()
-                        : o.getClass();
-        Class<?> thisEffectiveClass =
-                this instanceof HibernateProxy
-                        ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass()
-                        : this.getClass();
-        if (thisEffectiveClass != oEffectiveClass) return false;
-        InstanceEntity instance = (InstanceEntity) o;
-        return getId() != null && Objects.equals(getId(), instance.getId());
-    }
+	@Override
+	public final boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null) return false;
+		Class<?> oEffectiveClass =
+				o instanceof HibernateProxy
+						? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass()
+						: o.getClass();
+		Class<?> thisEffectiveClass =
+				this instanceof HibernateProxy
+						? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass()
+						: this.getClass();
+		if (thisEffectiveClass != oEffectiveClass) return false;
+		InstanceEntity instance = (InstanceEntity) o;
+		return getId() != null && Objects.equals(getId(), instance.getId());
+	}
 
-    @Override
-    public final int hashCode() {
-        return this instanceof HibernateProxy
-                ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode()
-                : getClass().hashCode();
-    }
+	@Override
+	public final int hashCode() {
+		return this instanceof HibernateProxy
+				? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode()
+				: getClass().hashCode();
+	}
 }
