5185e1041c6b474ecdd9962c5e5fb4a83bd77162
belljun3395 <195850@jnu.ac.kr>
2025-06-02 17:45:38 +0900

    feat: BrowseInstanceUseCase API 개발

--- src/main/java/com/okestro/resource/server/controller/ServerController.java
+++ src/main/java/com/okestro/resource/server/controller/ServerController.java
@@ -1,9 +1,14 @@
 package com.okestro.resource.server.controller;
 
+import static com.okestro.resource.server.application.dto.BrowseInstanceUseCaseDto.toPage;
+
+import com.okestro.resource.server.application.BrowseInstanceUseCase;
 import com.okestro.resource.server.application.FindInstanceUseCase;
+import com.okestro.resource.server.application.dto.BrowseInstanceUseCaseDto;
 import com.okestro.resource.server.application.dto.FindInstanceUseCaseDto;
 import com.okestro.resource.support.web.ApiResponse;
 import com.okestro.resource.support.web.ApiResponseGenerator;
+import com.okestro.resource.support.web.Page;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class ServerController {
 	private final FindInstanceUseCase findInstanceUseCase;
+	private final BrowseInstanceUseCase browseInstanceUseCase;
 
 	@GetMapping("/instances/{instanceId}")
 	public ApiResponse<ApiResponse.SuccessBody<FindInstanceUseCaseDto.FindInstanceUseCaseOut>>
@@ -20,5 +26,15 @@ public class ServerController {
 		FindInstanceUseCaseDto.FindInstanceUseCaseOut useCaseOut =
 				findInstanceUseCase.execute(FindInstanceUseCaseDto.in(instanceId));
 		return ApiResponseGenerator.success(useCaseOut, HttpStatus.OK);
+	}
+
+	@GetMapping("/instances")
+	public ApiResponse<Page<BrowseInstanceUseCaseDto.BrowseInstanceUseCaseOut.InstanceDto>>
+			browseInstances(
+					@RequestParam(value = "page", defaultValue = "0") Long pageNumber,
+					@RequestParam(value = "size", defaultValue = "10") Integer pageSize) {
+		BrowseInstanceUseCaseDto.BrowseInstanceUseCaseOut useCaseOut =
+				browseInstanceUseCase.execute(BrowseInstanceUseCaseDto.in(pageNumber, pageSize));
+		return ApiResponseGenerator.success(toPage(useCaseOut), HttpStatus.OK);
 	}
 }
