eec0602285ceb932c14271963393252171a9a455
belljun3395 <195850@jnu.ac.kr>
2025-06-02 17:45:21 +0900

    feat: FindInstanceUseCase API 개발

--- src/main/java/com/okestro/resource/server/controller/ServerController.java
+++ src/main/java/com/okestro/resource/server/controller/ServerController.java
@@ -1,10 +1,24 @@
 package com.okestro.resource.server.controller;
 
+import com.okestro.resource.server.application.FindInstanceUseCase;
+import com.okestro.resource.server.application.dto.FindInstanceUseCaseDto;
+import com.okestro.resource.support.web.ApiResponse;
+import com.okestro.resource.support.web.ApiResponseGenerator;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api/v1/servers")
 @RequiredArgsConstructor
 @RestController
-public class ServerController {}
+public class ServerController {
+	private final FindInstanceUseCase findInstanceUseCase;
+
+	@GetMapping("/instances/{instanceId}")
+	public ApiResponse<ApiResponse.SuccessBody<FindInstanceUseCaseDto.FindInstanceUseCaseOut>>
+			findInstance(@PathVariable Long instanceId) {
+		FindInstanceUseCaseDto.FindInstanceUseCaseOut useCaseOut =
+				findInstanceUseCase.execute(FindInstanceUseCaseDto.in(instanceId));
+		return ApiResponseGenerator.success(useCaseOut, HttpStatus.OK);
+	}
+}
